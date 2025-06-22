06589ec0b8b9b3d80ebcf9c06538be20729c1b7f
vitorcradi <43970478+vitorcradi@users.noreply.github.com>
2025-06-22 19:34:44 -0300

    fix(17-3-custom-action-docker): testing my python file

--- .github/actions/docker-ping-url/main.py
+++ .github/actions/docker-ping-url/main.py
@@ -9,10 +9,12 @@ def run():
 
 def ping_url(url, delay, max_trials):
   curl_command = f"curl {url} -is | grep '^HTTP\/' | cut -c 10-12"
+  curl_command1 = "curl -s -o /dev/null -w '%{http_code}' " + url
   command_result = subprocess.check_call(curl_command, shell=True, text=True)
   print(curl_command)
+  print(curl_command1)
   print(command_result)
-  sleep_command = "sleep ${delay}"
+  sleep_command = f"sleep {delay}"
   trials = 1
   while trials <= max_trials:
     if subprocess.check_output(curl_command, shell=True, text=True) != 200:
