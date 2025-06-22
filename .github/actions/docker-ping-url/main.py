52e2c4d716987b22f872d6e5e818af7723f92ed0
vitorcradi <43970478+vitorcradi@users.noreply.github.com>
2025-06-22 16:51:34 -0300

    Adding logic to main.py

--- .github/actions/docker-ping-url/main.py
+++ .github/actions/docker-ping-url/main.py
@@ -1,2 +1,27 @@
+import os
+import subprocess
+
+def run():
+  url = getenv(url)
+  delay = getenv(delay)
+  max_trials = getenv(max_trials)
+  ping_url(url, delay, max_trials)
+
+def ping_url(url, delay, max_trials):
+  curl_command = "curl ${url}"
+  sleep_command = "sleep ${delay}"
+  trials = 1
+  while trials <= max_trials:
+    if subprocess.check_output(curl_command, shell=True, text=True) != 200:
+      subprocess.call(sleep_command)
+      trials =+ 1
+    else:
+      return('True')
+      break
+  if trials > max_trials:
+    return('False')
+    
+
 if __name__ == "__main__":
-  print("Hello world")
+  print("ping test")
+  run()
