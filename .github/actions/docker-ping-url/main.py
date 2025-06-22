c8e8dbcbdebf4f95602fc8757f8a77145666f0aa
vitorcradi <43970478+vitorcradi@users.noreply.github.com>
2025-06-22 19:26:27 -0300

    fix(17-3-custom-action-docker): testing my python file

--- .github/actions/docker-ping-url/main.py
+++ .github/actions/docker-ping-url/main.py
@@ -1,52 +1,3 @@
-# import os
-# import requests
-# import time
-
-
-# def set_output(file_path, key, value):
-#     with open(file_path, 'a') as file:
-#         print(f'{key}={value}', file=file)
-
-
-# def ping_url(url, delay, max_trials):
-#     trials = 0
-
-#     while trials < max_trials:
-#         try:
-#             response = requests.get(url)
-#             if response.status_code == 200:
-#                 print(f"Website {url} is reachable.")
-#                 return True
-#         except requests.ConnectionError:
-#             print(f"Website {url} is unreachable. Retrying in {delay} seconds...")
-#             time.sleep(delay)
-#             trials += 1
-#         except requests.exceptions.MissingSchema:
-#             print(f"Invalid URL format: {url}. Make sure the URL has a valid schema (e.g., http:// or https://)")
-#             return False
-    
-#     return False
-
-
-# def run():
-#     website_url = os.getenv("INPUT_URL")
-#     delay = int(os.getenv("INPUT_DELAY"))
-#     max_trials = int(os.getenv("INPUT_MAX_TRIALS"))
-
-#     website_reachable = ping_url(website_url, delay, max_trials)
-
-#     set_output(os.getenv('GITHUB_OUTPUT'), 'url-reachable', website_reachable)
-    
-#     if not website_reachable:
-#         raise Exception(f"Website {website_url} is malformed or unreachable.")
-
-#     print(f"Website {website_url} is reachable.")
-
-
-# if __name__ == "__main__":
-#     run()
-
-
 from os import getenv
 import subprocess
 
@@ -57,15 +8,16 @@ def run():
   ping_url(url, delay, max_trials)
 
 def ping_url(url, delay, max_trials):
-  curl_command = "curl " + url
-  curl_command1 = f"curl {url}"
+  curl_command = f"curl {url} -is | grep '^HTTP\/' | cut -c 10-12"
+  command_result = subprocess.check_call(curl_command, shell=True, text=True)
   print(curl_command)
-  print(curl_command1)
+  print(command_result)
   sleep_command = "sleep ${delay}"
   trials = 1
   while trials <= max_trials:
     if subprocess.check_output(curl_command, shell=True, text=True) != 200:
       subprocess.call(sleep_command)
+      subprocess.check_call(curl_command, shell=True, text=True)
       trials += 1
     else:
       return('True')
