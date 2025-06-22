57a52a98898e80e463cc5bcdc59f3b0785c439b5
vitorcradi <43970478+vitorcradi@users.noreply.github.com>
2025-06-22 18:56:49 -0300

    fix(17-3-custom-action-docker): testing my python file

--- .github/actions/docker-ping-url/main.py
+++ .github/actions/docker-ping-url/main.py
@@ -1,76 +1,76 @@
-import os
-import requests
-import time
+# import os
+# import requests
+# import time
 
 
-def set_output(file_path, key, value):
-    with open(file_path, 'a') as file:
-        print(f'{key}={value}', file=file)
+# def set_output(file_path, key, value):
+#     with open(file_path, 'a') as file:
+#         print(f'{key}={value}', file=file)
 
-
-def ping_url(url, delay, max_trials):
-    trials = 0
-
-    while trials < max_trials:
-        try:
-            response = requests.get(url)
-            if response.status_code == 200:
-                print(f"Website {url} is reachable.")
-                return True
-        except requests.ConnectionError:
-            print(f"Website {url} is unreachable. Retrying in {delay} seconds...")
-            time.sleep(delay)
-            trials += 1
-        except requests.exceptions.MissingSchema:
-            print(f"Invalid URL format: {url}. Make sure the URL has a valid schema (e.g., http:// or https://)")
-            return False
-    
-    return False
-
-
-def run():
-    website_url = os.getenv("INPUT_URL")
-    delay = int(os.getenv("INPUT_DELAY"))
-    max_trials = int(os.getenv("INPUT_MAX_TRIALS"))
-
-    website_reachable = ping_url(website_url, delay, max_trials)
-
-    set_output(os.getenv('GITHUB_OUTPUT'), 'url-reachable', website_reachable)
-    
-    if not website_reachable:
-        raise Exception(f"Website {website_url} is malformed or unreachable.")
-
-    print(f"Website {website_url} is reachable.")
-
-
-if __name__ == "__main__":
-    run()
-
-
-# from os import getenv
-# import subprocess
-
-# def run():
-#   url = getenv("INPUT_URL")
-#   delay = getenv("INPUT_DELAY")
-#   max_trials = getenv("INPUT_MAX_TRIALS")
-#   ping_url(url, delay, max_trials)
 
 # def ping_url(url, delay, max_trials):
-#   curl_command = "curl ${url}"
-#   sleep_command = "sleep ${delay}"
-#   trials = 1
-#   while trials <= max_trials:
-#     if subprocess.check_output(curl_command, shell=True, text=True) != 200:
-#       subprocess.call(sleep_command)
-#       trials += 1
-#     else:
-#       return('True')
-#       break
-#   if trials > max_trials:
-#     return('False')
+#     trials = 0
+
+#     while trials < max_trials:
+#         try:
+#             response = requests.get(url)
+#             if response.status_code == 200:
+#                 print(f"Website {url} is reachable.")
+#                 return True
+#         except requests.ConnectionError:
+#             print(f"Website {url} is unreachable. Retrying in {delay} seconds...")
+#             time.sleep(delay)
+#             trials += 1
+#         except requests.exceptions.MissingSchema:
+#             print(f"Invalid URL format: {url}. Make sure the URL has a valid schema (e.g., http:// or https://)")
+#             return False
     
+#     return False
+
+
+# def run():
+#     website_url = os.getenv("INPUT_URL")
+#     delay = int(os.getenv("INPUT_DELAY"))
+#     max_trials = int(os.getenv("INPUT_MAX_TRIALS"))
+
+#     website_reachable = ping_url(website_url, delay, max_trials)
+
+#     set_output(os.getenv('GITHUB_OUTPUT'), 'url-reachable', website_reachable)
+    
+#     if not website_reachable:
+#         raise Exception(f"Website {website_url} is malformed or unreachable.")
+
+#     print(f"Website {website_url} is reachable.")
+
 
 # if __name__ == "__main__":
-#   print("ping test")
-#   run()
+#     run()
+
+
+from os import getenv
+import subprocess
+
+def run():
+  url = getenv("INPUT_URL")
+  delay = getenv("INPUT_DELAY")
+  max_trials = getenv("INPUT_MAX_TRIALS")
+  ping_url(url, delay, max_trials)
+
+def ping_url(url, delay, max_trials):
+  curl_command = "curl ${url}"
+  sleep_command = "sleep ${delay}"
+  trials = 1
+  while trials <= max_trials:
+    if subprocess.check_output(curl_command, shell=True, text=True) != 200:
+      subprocess.call(sleep_command)
+      trials += 1
+    else:
+      return('True')
+      break
+  if trials > max_trials:
+    return('False')
+    
+
+if __name__ == "__main__":
+  print("ping test")
+  run()
